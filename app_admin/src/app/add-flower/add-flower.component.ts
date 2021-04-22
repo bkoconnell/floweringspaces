befc88ecbaac875a9fdfbbe7eaa90e0c955e98c2
Brendan OConnell <bkoconnell@gmail.com>
2021-04-21 22:17:04 -0400

    addFlower logic & form added to app_admin frontend

--- app_admin/src/app/add-flower/add-flower.component.ts
+++ app_admin/src/app/add-flower/add-flower.component.ts
@@ -1,15 +1,65 @@
+// import Angular modules
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+/* Angular's 'forms' module allows code to access the data using "data binding":
+    The JavaScript variables are “bound” to the HTML form controls 
+    with Angular’s forms library taking care of moving data 
+    back and forth between the view (HTML) and the model (JavaScript).
+*/
+
+// import service
+import { FlowerDataService } from '../services/flower-data.service';
+
 
 @Component({
   selector: 'app-add-flower',
   templateUrl: './add-flower.component.html',
   styleUrls: ['./add-flower.component.css']
 })
+
 export class AddFlowerComponent implements OnInit {
 
-  constructor() { }
+  addForm: FormGroup; // to track value & validity state of a group of FormControl instances
+  submitted = false;  // set Boolean
+
+  // constructor: define parameters to inject an instance within this class
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private flowerService: FlowerDataService
+  ) { }
 
   ngOnInit() {
+    // setup a blank form on initialize
+    this.addForm = this.formBuilder.group({
+      _id: [],
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      scientific: ['', Validators.required],
+      type: ['', Validators.required],
+      size: ['', Validators.required],
+      price: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required],
+    })
   }
+
+  // user submission function ('Save' button)
+  onSubmit() {
+    this.submitted = true;
+    // validate submission
+    if (this.addForm.valid) {
+      // invoke addFlower method
+      this.flowerService.addFlower(this.addForm.value)
+        .then(data => {
+          console.log(data);          // send data to console
+          this.router.navigate(['']); // router navigation
+        });
+    }
+  }
+
+  // get the form short-name to access the form fields
+  get f() { return this.addForm.controls; }
 
 }
--- app_admin/src/app/services/flower-data.service.ts
+++ app_admin/src/app/services/flower-data.service.ts
@@ -8,24 +8,40 @@ import { Flower } from '../models/flower';
 @Injectable()
 export class FlowerDataService {
 
-  constructor(private http: Http) { } // receive in Http module, assigned to http
+  // constructor: define parameters to inject an instance within this class
+  constructor(private http: Http) { }
 
-  private apiBaseUrl = 'http://localhost:3000/api/';  // assignment for API URL
+  // URL variables
+  private apiBaseUrl = 'http://localhost:3000/api/';
+  private flowerUrl = `${this.apiBaseUrl}flowers/`;
+
+  // method to add a flower
+  public addFlower(formData: Flower): Promise<Flower> {
+    // console output
+    console.log('Inside FlowerDataService#addFlower');
+    // return logic
+    return this.http                                 // Use http module for .post() method below:
+      .post(this.flowerUrl, formData)                // Pass form data in request body
+      .toPromise()                                   // Convert observable to promise
+      .then(response => response.json() as Flower[]) // Success handler: return promise value (response)
+      .catch(this.handleError);                      // or, Error handler (invoke handleError method)
+  }
 
   // method to return an array of flowers
   public getFlowers(): Promise<Flower[]> {
     // console output
     console.log('Inside FlowerDataService#getFlowers');
     // return logic
-    return this.http                                // Use http module for .get() method below:
-      .get(`${this.apiBaseUrl}flowers`)               // Evaluate templated string (http://localhost:3000/flowers)
-      .toPromise()                                  // Send response to Promise
-      .then(response => response.json() as Flower[])  // Response will be array of flowers
-      .catch(this.handleError);                     // Call method to handle errors
+    return this.http                                 // Use http module for .get() method below:
+      .get(this.flowerUrl)                           // Evaluate URL
+      .toPromise()                                   // Convert observable to promise
+      .then(response => response.json() as Flower[]) // Success handler: return promise value (response)
+      .catch(this.handleError);                      // or, Error handler (invoke handleError method)
   }
 
   // method to handle error
   private handleError(error: any): Promise<any> {
+    // FIXME: Replace with more appropriate console error message
     console.error('Something has gone wrong', error); // for demo purposes only
     return Promise.reject(error.message || error);
   }
