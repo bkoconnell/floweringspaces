caaa77dc4f604fe937dba93b76a1333d321fe4c6
Brendan OConnell <bkoconnell@gmail.com>
2021-04-23 06:12:59 -0400

    updated app-router module on front-end with delete-flower

--- app_admin/src/app/app-router.module.ts
+++ app_admin/src/app/app-router.module.ts
@@ -5,11 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { FlowerListingComponent } from './flower-listing/flower-listing.component';
 import { AddFlowerComponent } from './add-flower/add-flower.component';
 import { EditFlowerComponent } from './edit-flower/edit-flower.component';
+import { DeleteFlowerComponent } from './delete-flower/delete-flower.component';
 
 // define & map routes
 const routes: Routes = [
     { path: 'add-flower', component: AddFlowerComponent},     // add-flower route
     { path: 'edit-flower', component: EditFlowerComponent},   // edit-flower route
+    { path: 'delete-flower', component: DeleteFlowerComponent},   // delete-flower route
     // default path (when none is specified):
     { path: '', component: FlowerListingComponent, pathMatch: 'full' }
 ]
