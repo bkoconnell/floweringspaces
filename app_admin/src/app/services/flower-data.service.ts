c5b62e10f7f2785b26217e0f4bd705af227a64eb
Brendan OConnell <bkoconnell@gmail.com>
2021-04-23 04:34:50 -0400

    add deleteFlower method to frontend flower-data service

--- app_admin/src/app/services/flower-data.service.ts
+++ app_admin/src/app/services/flower-data.service.ts
@@ -1,8 +1,9 @@
 /**
  * This is the frontend (client) service logic
- * for sending request methods to the API endpoint
- * and returning the response from the API endpoint
+ * for sending HTTP requests (method calls) to the REST API endpoint
+ * and returning the API's response to the requester
  */
+
 
 // import Angular modules
 import { Injectable } from '@angular/core';
@@ -18,61 +19,72 @@ export class FlowerDataService {
   // define constructor parameters (to inject an instance when class is instantiated)
   constructor(private http: Http) { }
 
-  // Build URL's for API and Endpoints
-  private apiBaseUrl = 'http://localhost:3000/api/'; // API
+  // Build URL's for REST API and API Endpoint
+  private apiBaseUrl = 'http://localhost:3000/api/'; // REST API
   private flowerUrl = `${this.apiBaseUrl}flowers/`;  // API Endpoint (flowers collection)
 
   // method to add a flower to database through API
   public addFlower(formData: Flower): Promise<Flower> {
     // console output
     console.log('Inside FlowerDataService#addFlower');
-    // HTTP request & API response
+    // logic for HTTP request call & API response handling
     return this.http
-      .post(this.flowerUrl, formData)                // POST call to API endpoint, pass form data as request body
+      .post(this.flowerUrl, formData)                // POST call to API w/ request body (form data)
       .toPromise()                                   // Convert observable response to a promise
-      .then(response => response.json() as Flower[]) // Success handler: return the promise response (new data object as seen in db)
-      .catch(this.handleError);                      // or, Error handler (invoke handleError method)
+      .then(response => response.json() as Flower[]) // Set promise response to single instance for return (new data object as seen in db)
+      .catch(this.handleError);                      // Error handler (invoke handleError method)
   }
 
   // method to get a single flower
   public getFlower(flowerCode: string): Promise<Flower> {
     // console output
     console.log('Inside FlowerDataService#getFlower(flowerCode)');
-    // HTTP request & API response
+    // logic for HTTP request call & API response handling
     return this.http
-      .get(this.flowerUrl + flowerCode)            // GET call to API endpoint w/ flower code parameter
+      .get(this.flowerUrl + flowerCode)            // GET call to API w/ URL parameter (flower code)
       .toPromise()                                 // Convert observable response to a promise
-      .then(response => response.json() as Flower) // Success handler: return the promise response (data object answering the request)
-      .catch(this.handleError);                    // or, Error handler (invoke handleError method)
+      .then(response => response.json() as Flower) // Set promise response to single instance for return (data object answering the request)
+      .catch(this.handleError);                    // Error handler (invoke handleError method)
   }
 
   // method to return an array of flowers
   public getFlowers(): Promise<Flower[]> {
     // console output
     console.log('Inside FlowerDataService#getFlowers');
-    // HTTP request & API response
+    // logic for HTTP request call & API response handling
     return this.http
-      .get(this.flowerUrl)                           // GET call to API endpoint
+      .get(this.flowerUrl)                           // GET call to API
       .toPromise()                                   // Convert observable response to a promise
-      .then(response => response.json() as Flower[]) // Success handler: return the promise response (data object answering the request)
-      .catch(this.handleError);                      // or, Error handler (invoke handleError method)
+      .then(response => response.json() as Flower[]) // Set promise response to single instance for return (data object answering the request)
+      .catch(this.handleError);                      // Error handler (invoke handleError method)
   }
 
   // method to update a flower
   public updateFlower(formData: Flower): Promise<Flower> {
     // console output
-    console.log('Inside FlowerDataService#upateFlower');
+    console.log('Inside FlowerDataService#updateFlower');
     console.log(formData); // dump form data to console (debugging)
-    // HTTP request & API response
+    // logic for HTTP request call & API response handling
     return this.http
-      .put(this.flowerUrl + formData.code, formData) // PUT call to API endpoint w/ object data, pass flower code
+      .put(this.flowerUrl + formData.code, formData) // PUT call to API w/ URL parameter (flower code) & request body (form data)
       .toPromise()                                   // Convert observable response to a promise
-      .then(response => response.json() as Flower[]) // Success handler: return the promise response (updated data object as seen in the db)
-      .catch(this.handleError);                      // or, Error handler (invoke handleError method)
+      .then(response => response.json() as Flower[]) // Set promise response to single instance for return (updated data object as seen in the db)
+      .catch(this.handleError);                      // Error handler (invoke handleError method)
   }
 
 
   // FIXME:  Add Delete method (comments for response: Null)
+  // method to delete a flower
+  public deleteFlower(flowerCode: string): Promise<any> {
+    // console output
+    console.log('Inside FlowerDataService#deleteFlower');
+    // logic for HTTP request call & API response handling
+    return this.http
+      .delete(this.flowerUrl + flowerCode)           // DELETE call to API w/ URL parameter (flower code)
+      .toPromise()                                   // Convert observable response to a promise
+      .then(response => response.json() as null)     // Set promise response to single instance for return ()
+      .catch(this.handleError);                      // Error handler (invoke handleError method)
+  }
 
 
   // method to handle error
